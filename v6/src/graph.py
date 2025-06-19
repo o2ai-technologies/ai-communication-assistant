0b34dd17f0057b19f0f16139c805f8bb120cfb63
sudo <sudo@o2.ai>
2025-06-19 11:43:07 +0300

    google's deep research integration WIP

--- v6/src/graph.py
+++ v6/src/graph.py
@@ -1,19 +1,18 @@
 from langgraph.graph import StateGraph, END, START
 from src.state import GraphState, memory
 from src.interview_graph import interview_graph
-# from src.deep_research.graph import deep_research_graph
+from src.deep_research.graph import deep_research_graph
 
 
 builder = StateGraph(GraphState)
 
 builder.add_node("interview_graph", interview_graph)
-# builder.add_node("deep_research_graph", deep_research_graph)
+builder.add_node("deep_research_graph", deep_research_graph)
 
 
 builder.add_edge(START, "interview_graph")
-# builder.add_edge("interview_graph", "deep_research_graph")
-# builder.add_edge("deep_research_graph", END)
-builder.add_edge("interview_graph", END)
+builder.add_edge("interview_graph", "deep_research_graph")
+builder.add_edge("deep_research_graph", END)
 
 graph = builder.compile(checkpointer=memory)
 # graph = builder.compile()
